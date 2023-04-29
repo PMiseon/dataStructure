80a3daa13527b4bed4aebf762d7f50a1c8c3e7e2
Miseon Park <106222104+PMiseon@users.noreply.github.com>
2023-04-30 02:07:45 +0900

    Create week9-2.c

---
+++ 9주차_실습/week9-2.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#pragma warning (disable : 4996)
+
+char pop(char *stack, int *t) {
+	if (*t == -1) {
+		//emptyStackException;
+		//*tmp = '-1';
+		return '0'; 
+	}
+
+	
+	*t = *t - 1;
+
+	//print(stack, t);
+	return stack[*t+1];
+}
+
+void push(char *stack, int *t, char e) {
+	*t = *t + 1;
+	stack[*t] = e;
+	//print(stack, t);
+	return;
+}
+
+void print(char *stack, int *t) {
+	int i;
+	for (i = *t; i > -1; i--) {
+		printf("%c", stack[i]);
+	}
+	printf("\n");
+}
+
+
+int main() {
+	char s[1001] = { 0 };
+
+	//initStack 
+	//char *stack = malloc(sizeof(char) * 500);
+	char stack[500] = { 0 };
+	int t = -1; 
+
+	//scanf("%s", s);
+	gets(s);
+	int len = strlen(s);
+
+	//printf("%d\n%s", len, s);
+
+	int i;
+	char tmp = NULL; 
+	int count = 0; 
+	int flag = 0; 
+	// flag == 0 -> OK_count, flag == 1-> Wrong_count
+
+	for (i = 0; i < len; i++) {
+
+		if (s[i] == '{') {
+			count++;
+			push(&stack, &t, '{');
+		}
+
+		else if (s[i] == '(') {
+			count++;
+			push(&stack, &t, '(');
+		}
+
+		else if (s[i] == '[') {
+			count++;
+			push(&stack, &t, '[');
+		}
+
+		else if (s[i] == '}') {
+			count++;
+			tmp = pop(&stack, &t);
+			if (tmp != '{') {
+				flag = 1;
+			}
+		}
+
+		else if (s[i] == ')') {
+			count++;
+			tmp = pop(&stack, &t);
+			if (tmp != '(')
+				flag = 1;
+		}
+
+		else if (s[i] == ']') {
+			count++;
+			tmp = pop(&stack, &t);
+			if (tmp != '[')
+				flag = 1;
+		}
+	}
+
+	if (t != -1) {
+		printf("Wrong_%d\n", count);
+		return 0;
+	}
+
+	if (flag == 0) {
+		printf("OK_%d\n", count);
+	}
+
+	else if (flag == 1) {
+		printf("Wrong_%d\n", count);
+	}
+}
