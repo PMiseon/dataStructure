e890639b903359ef81a0c98e7ca33e48985eb2ad
Miseon Park <106222104+PMiseon@users.noreply.github.com>
2023-05-02 11:23:21 +0900

    Update week9-1.c

--- 9주차_실습/week9-1.c
+++ 9주차_실습/week9-1.c
@@ -10,13 +10,7 @@ void fullStackException() {
 	return;
 }
 
-void initStack(char *stack, int *t, int N) {
-	stack = (char*)malloc(sizeof(char)*(N + 1));
-	*t = -1;
-	return;
-}
-
-void push(char *stack, int *t, int N, char e) {
+void push(char stack[], int *t, int N, char e) {
 	if (*t == N - 1) {
 		fullStackException();
 		return;
@@ -41,7 +35,7 @@ void pop(char *stack, int *t) {
 	*t = *t - 1;
 }
 
-void duplicate(char *stack, int *t, int N) {
+void duplicate(char stack[], int *t, int N) {
 	/* stack의 top에 있는 원소를 pop해서 두 번 push한다.
 	stack이 이미 꽉 차 있으면 “Stack FULL”을 출력한다. */
 	if (*t == N - 1) {
@@ -49,15 +43,15 @@ void duplicate(char *stack, int *t, int N) {
 		return;
 	}
 
-	char c = stack[*t]; //<- top에 있는 원소
-	pop(stack, t); 
+	char c = stack[*t];
+	pop(stack, t);
 	push(stack, t, N, c);
 	push(stack, t, N, c);
 	return;
 }
 
 
-void peek(char *stack, int *t) {
+void peek(char stack[], int *t) {
 	/*stack의 top에 있는 원소를 화면에 출력한다.
 	stack은 변화하지 않는다. stack이 비어 있으면 “Stack Empty”를 출력한다.*/
 	if (*t == -1) {
@@ -67,25 +61,24 @@ void peek(char *stack, int *t) {
 	printf("%c\n", stack[*t]);
 }
 
-void upRotate(char *stack, int *t, int n) {
-	if (n - 1 > *t) 
+void upRotate(char stack[], int *t, int n) {
+	if (n - 1 > *t)
 		return;
 
-	char tmp = stack[*t]; //elem1 
+	char tmp = stack[*t];
 	int i;
 	for (i = *t - 1; i > *t - n; i--) {
 		stack[i + 1] = stack[i];
 	}
-
-	stack[*t - n + 1] = tmp; //elem1 처리 
+	stack[*t - n + 1] = tmp;
 }
 
-void downRotate(char *stack, int *t, int n) {
-	char tmp = stack[*t - n + 1]; 
+void downRotate(char stack[], int *t, int n) {
 
 	if (n - 1 > *t)
 		return;
 
+	char tmp = stack[*t - n + 1];
 	int i;
 	for (i = *t - n + 2; i < *t + 1; i++) {
 		stack[i - 1] = stack[i];
@@ -93,15 +86,8 @@ void downRotate(char *stack, int *t, int n) {
 	stack[*t] = tmp;
 }
 
-//void check(char *stack, int *t) {
-//	int i;
-//	for (i = 0; i <= *t; i++) {
-//		printf("%c", stack[i]);
-//	}
-//	printf("  check\n");
-//}
 
-void print(char *stack, int *t) {
+void print(char stack[], int *t) {
 	int i;
 	for (i = *t; i > -1; i--) {
 		printf("%c", stack[i]);
@@ -111,11 +97,13 @@ void print(char *stack, int *t) {
 }
 
 int main() {
-	char *stack = NULL;
+
 	int N, t;
 	scanf("%d", &N);
-	initStack(&stack, &t, N);
 
+	//init Stack 
+	char *stack = malloc(sizeof(char)*(N + 1));
+	t = -1;
 
 	int k, i;
 	scanf("%d", &k);
@@ -130,34 +118,36 @@ int main() {
 
 		if (strcmp(cmd, "PUSH") == 0) {
 			scanf("%c", &tmp);
-			push(&stack, &t, N, tmp);
+			push(stack, &t, N, tmp);
 		}
 
 		else if (strcmp(cmd, "POP") == 0) {
-			pop(&stack, &t);
+			pop(stack, &t);
 		}
 
 		else if (strcmp(cmd, "PRINT") == 0) {
-			print(&stack, &t);
+			print(stack, &t);
 		}
 
 		else if (strcmp(cmd, "UpR") == 0) {
 			scanf("%d", &n);
-			upRotate(&stack, &t, n);
+			upRotate(stack, &t, n);
 		}
 
 		else if (strcmp(cmd, "DownR") == 0) {
 			scanf("%d", &n);
-			downRotate(&stack, &t, n);
+			downRotate(stack, &t, n);
 		}
 
 		else if (strcmp(cmd, "PEEK") == 0) {
-			peek(&stack, &t);
+			peek(stack, &t);
 		}
 
 		else if (strcmp(cmd, "DUP") == 0) {
-			duplicate(&stack, &t, N);
+			duplicate(stack, &t, N);
 		}
 	}
 
+	free(stack);
+	//stack 동적할당해제하기 
 }
